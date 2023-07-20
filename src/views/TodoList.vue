a34cc8ff24a87318767daff2208a517400e95d9d
Promise-Devotion <sijun0224@msn.com>
2023-07-20 11:28:24 +0800

    组件使用setup语法糖

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -11,37 +11,33 @@
     <button type="button" @click="getlist">点击</button>
   </div>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
 import { reactive } from "vue";
 import billService from "../api/bill";
 
-export default {
-  setup() {
-    const state = reactive<StateConfig>({
-      arr: [],
-    });
-    interface StateConfig {
-      arr: Item[];
+const state = reactive<StateConfig>({
+  arr: [
+    {
+      bill_id: "12133",
+      bill_name: "旅游",
     }
-    interface Item {
-      bill_id: string;
-      bill_name: string;
-    }
-    function getlist() {
-      // let url = `http://localhost:5000/api/users/userlist?name=jim`;
-      billService.getBillList({ id: "1235" }).then((res) => {
-        console.log(res);
-        state.arr = res.data;
-        console.log(res.data);
-      });
-    }
-
-    return {
-      state,
-      getlist,
-    };
-  },
-};
+  ],
+});
+interface StateConfig {
+  arr: Item[];
+}
+interface Item {
+  bill_id: string;
+  bill_name: string;
+}
+function getlist() {
+  // let url = `http://localhost:5000/api/users/userlist?name=jim`;
+  billService.getBillList({ id: "1235" }).then((res) => {
+    console.log(res);
+    state.arr = res.data;
+    console.log(res.data);
+  });
+}
 </script>
 
 <style scoped>
